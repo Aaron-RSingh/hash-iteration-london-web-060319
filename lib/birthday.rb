1c7d76cb9eb4fcb14eb77de845af41ed967bace3
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-30 12:56:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,13 +11,13 @@ birthday_kids.each do |key, value|
 end
 end
 
-def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each { |key, value|
-    if value <= 12
-      puts "Happy Birthday #{key}! You are now #{value} years old!"
-    else 
-      puts "You are too old for this."
-end
-}
+#def age_appropriate_birthday(birthday_kids)
+#  birthday_kids.each { |key, value|
+#    if value <= 12
+#      puts "Happy Birthday #{key}! You are now #{value} years old!"
+#    else 
+#      puts "You are too old for this."
+#end
+#}
 
-end
+#end
